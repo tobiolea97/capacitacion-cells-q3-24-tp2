9836c6a0383dc21d3d52afc821df9fd530925cef
Tobias Olea <tobiolea97@gmail.com>
2024-08-28 10:37:39 -0300

    Avances en alert component. Mostrando mensaje en pagina principal.

---
+++ src/components/alert-component.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from "lit";
+
+export class AlertComponent extends LitElement {
+  static get properties() {
+    return {
+      message: { type: String },
+      type: { type: String }, // success, error, warning, info
+    };
+  }
+
+  static styles = [
+    css`
+      :host {
+        display: block;
+      }
+
+      .alert {
+        padding: 20px;
+        background-color: #f44336;
+        color: white;
+        margin-bottom: 15px;
+      }
+
+      .alert.success {
+        background-color: #4caf50;
+      }
+      .alert.info {
+        background-color: #2196f3;
+      }
+      .alert.warning {
+        background-color: #ff9800;
+      }
+    `,
+  ];
+
+  constructor() {
+    super();
+    this.message = "";
+    this.type = "info";
+  }
+
+  render() {
+    return html` <div class="alert ${this.type}">${this.message}</div> `;
+  }
+}
+customElements.define("alert-component", AlertComponent);
+
